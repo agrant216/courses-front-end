cf659e21e26924bc085894e3454613abbae5fc60
agrant216 <agrant12@kent.edu>
2021-11-01 12:33:33 -0400

    Adding course list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CourseCatalogComponent } from './components/course-catalog/course-catalog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseCatalogComponent
   ],
   imports: [
     BrowserModule,
